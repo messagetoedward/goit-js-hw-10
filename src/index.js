4d05795297797a3f1ca6a31ce2c6a532bb7b417c
Edward Zaplotynskyi <edward.downtown@gmail.com>
2021-11-18 23:57:29 +0200

    final touches

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
-import { fetchCountries } from "./fetchCountries.1";
+import { fetchCountries } from "./fetchCountries";
 import { debounce, throttle } from 'lodash';
+import Notiflix from 'notiflix';
 
 const searchBox = document.querySelector('#search-box');
 const infoEl = document.querySelector('.country-info');
@@ -9,24 +10,51 @@ const DEBOUNCE_DELAY = 300;
 
 searchBox.addEventListener('input', debounce(event => {
     event.preventDefault();
+    if (searchBox.value) {
     const name = searchBox.value.trim();
-    fetchCountries(name).then(showCountries).catch(error);
+    return fetchCountries(name).then(showCountries).catch(error);
+    } else {
+        countryEl.innerHTML = '';
+        infoEl.innerHTML = ''; }
+    
 }, DEBOUNCE_DELAY));
 
 function showCountries(countries) {
-    console.log(countries);
-    countries.map(country => console.log(country.flags.svg))
+        countryEl.innerHTML = '';
+        infoEl.innerHTML = '';
+    if (countries.length > 10) {
+        return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.', {timeout: 1000})
+    }
+    if (countries.length >= 2) {
+        markupTwoPlus(countries);
+    }
+    if (countries.length === 1) {
+        markupOne(countries);
+    }
+};
 
-    const markup =
-        `<h1></h1>
-    <li>Capital</li>
-    <li>Population</li>
-    <li>Languages</li>`;
+function markupTwoPlus(countries) {
+    const markupTwoPlus = countries.map(({ name, capital, population, flags, languages }) => {
+              return `<li><img src="${flags.svg}" alt="Flag of ${name.official}" style="height: 1em; width: 1em"> ${name.official}</li>`
+          }).join('');
+        countryEl.innerHTML = markupTwoPlus;
+};
+
+function markupOne(countries) {
+        countries.map(({ name, capital, population, flags, languages }) => {
+            const markup =
+            `<h1><img src="${flags.svg}" alt="Flag of ${name.official}" style="height: 1em; width: 1em"> ${name.official}</h1>
+        <li>Capital: ${capital}</li>
+        <li>Population: ${population}</li>
+        <li>Languages: ${Object.values(languages)}</li>`;
+            infoEl.innerHTML = markup;
+        })
+    
 }
 
-function error(error) {
-    console.log(error);
-}
+function error() {
+    return Notiflix.Notify.failure('Oops, there is no country with that name', {timeout: 1000});
+};
 
 // [{"capital":["Lima"],"languages":{"aym":"Aymara","que":"Quechua","spa":"Spanish"},"flag":"🇵🇪","population":32971846}] 
 
